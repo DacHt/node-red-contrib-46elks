1f40919f6774cec8701d74e4e5249f0d8b6dbf71
dacht <dacht@github.com>
2020-08-20 19:36:16 +0200

    add options for dryrun, flashsms and dontlog

--- send-sms.js
+++ send-sms.js
@@ -5,42 +5,48 @@ module.exports = function (RED) {
     
     
     function elksSMS(node, number, text) {
-        var postData = querystring.stringify ({
-            from: node.elks.from,
-            to: number,
-            message: text
-            //whendelivered: node.elks.feedbackURL
-        })
-        node.warn(postData)
-        
-        var key = new Buffer(node.elks.credentials.user + ':' + node.elks.credentials.password).toString('base64')
-        var postOptions = {
-            hostname: 'api.46elks.com',
-            path:     '/a1/SMS',
-            method:   'POST',
-            headers:  {
-              'Authorization': 'Basic ' + key
-            }
-        }
-        node.warn(postOptions)
+      
+      
+      
+      var postData = querystring.stringify ({
+          from: node.elks.from,
+          to: number,
+          message: text,
+          dryrun: node.dryrun?"yes":"no",
+          flashsms: node.flashsms?"yes":"no",
+          dontlog: node.dontlog?"message":"",
+          whendelivered: node.elks.feedbackURL
+      })
+      node.warn(postData)
+      
+      var key = new Buffer(node.elks.credentials.user + ':' + node.elks.credentials.password).toString('base64')
+      var postOptions = {
+          hostname: 'api.46elks.com',
+          path:     '/a1/SMS',
+          method:   'POST',
+          headers:  {
+            'Authorization': 'Basic ' + key
+          }
+      }
+      node.warn(postOptions)
 
-        // Start the web request.
-        var post_req = https.request(postOptions, function(response) {
-            var str = ''
-            response.on('data', (chunk) => {
-                str += chunk
-            })
-        
-            response.on('end', () => {
-                node.warn(str)
-            })
-        })
-        
-        // Send the real data away to the server.
-        post_req.write(postData)
+      // Start the web request.
+      var post_req = https.request(postOptions, function(response) {
+          var str = ''
+          response.on('data', (chunk) => {
+              str += chunk
+          })
+      
+          response.on('end', () => {
+              node.warn(str)
+          })
+      })
+      
+      // Send the real data away to the server.
+      post_req.write(postData)
 
-        // Finish sending the request.
-        post_req.end()
+      // Finish sending the request.
+      post_req.end()
     }
   
     function sendSMS(node, msg) {
@@ -55,6 +61,9 @@ module.exports = function (RED) {
       this.elks = RED.nodes.getNode(config.elks)
       this.message = config.message
       this.numbers = config.numbers
+      this.dryrun = config.dryrun
+      this.flashsms = config.flashsms
+      this.dontlog = config.dontlog
       this.on("input", sendSMS.bind(undefined, this))
     }
   
